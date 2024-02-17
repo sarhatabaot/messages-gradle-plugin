1df50553a39e3c24e963c0a3f2bb1d7c92c28087
Omer Oreg <omeroreg@gmail.com>
2024-02-17 19:04:49 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "messages-gradle-plugin"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("messages-core", "com.github.sarhatabaot:messages-core:1.6.0-beta-4")
+            library("messages-core", "com.github.sarhatabaot:messages-core:1.6.0-beta-5")
         }
     }
 }
